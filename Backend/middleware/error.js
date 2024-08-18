2d027f4e7c38069ad18a49d971e2de7df46faee2
Kuldip <kuldeepchudasama6999@gmail.com>
2024-08-18 11:59:25 +0530

    4

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal servar error";
 
+  // invaild mogo db id 
+  if(err.name == "CastError"){
+    const massage = "resouce not found due to invalid path";
+
+    err = new ErrorHandler(massage,400)
+  }
  
   res.json({
     code: err.statusCode,
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,14 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 
+
+// handlin uncaught erro like nothig defing but then console and use this 
+process.on("uncaughtException",(err)=>{
+    console.log("error: " + err);
+    console.log("shuting down the server due to the uncaught exception: ");
+    process.exit(1);
+})
+
 // Load environment variables from .env file
 dotenv.config({ path: './config/.env' });
 
@@ -10,6 +18,18 @@ connectDatabase();
 
 // Set a different port if 4000 is in use
 const port = process.env.PORT || 4001;
-app.listen(port, () => {
+ const server = app.listen(port, () => {
     console.log(`🚀 Listening on port ${port}`);
 });
+
+
+// unhanddled promis rejection in server meands string errorn in the server like mongodb link
+process.on("unhandledRejection",err =>{
+    console.log("Error" + err);
+    console.log("shuting down the server due to the unhandle promis rejection ")
+
+    server.close(()=>{
+        process.exit(1);
+    })
+
+}) 
